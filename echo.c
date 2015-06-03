f5109755c9450e9a049e78c65289b6b1b4652d43
Maout Du <maout.du@yopmail.com>
2015-06-03 18:22:56 +0200

    echo: better handling of separators
    fixes issue #2

--- echo.c
+++ echo.c
@@ -3,10 +3,9 @@
 int main (int argc, char *argv[]) {
 	if (++argv, --argc) {
 		while (putchar (*(*argv)++), **argv);
-		putchar (' ');
+		putchar (argc == 1 ? '\n' : ' ');
 		return main (argc, argv);
 	} else {
-		putchar ('\n');
 		return 1;
 	}
 }
