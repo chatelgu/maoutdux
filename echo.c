759fe3bd994a1af5e9abb26dc62783517ef2deac
Maout Du <maout.du@yopmail.com>
2015-06-03 17:00:47 +0200

    echo.c: clean the code
    - remove unnecessary parethesis
    - remove extra space to conform to the maoutdux style

--- echo.c
+++ echo.c
@@ -2,7 +2,7 @@
 
 int main (int argc, char *argv[]) {
 	if (++argv, --argc) {
-		while (putchar (*((*argv)++)), **argv );
+		while (putchar (*(*argv)++), **argv);
 		putchar (' ');
 		return main (argc, argv);
 	} else {
