54cef098166d03b9cbf0e5b2b3144bad2773c462
Guillaume Châtelet <guillaume.chatelet@orange.com>
2015-06-05 18:02:58 +0200

    Fix #1 Allow echo not to display a newline

--- echo.c
+++ echo.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
+	char endchar='\n';
+	if (!strcmp(argv[1],"-n")) {
+		--argc;
+		++argv;
+		endchar=' ';
+	}
 	while (--argc) {
 		fputs(*++argv, stdout);
-		putchar (argc == 1 ? '\n' : ' ');
+		putchar (argc == 1 ? endchar : ' ');
 	}
 	return 0;
 }
